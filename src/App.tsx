42a1b636e79c8e1c7d8f426c2a54f97eb2e8df6c
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-06-17 00:21:15 +0100

    Worked on folder structure and naming of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import AmountInput from './components/AmountInput/index';
+// import React, { useState } from 'react';
+
+import AmountInput from './components/AmountInput';
+import Currencies from './components/Currencies';
+import Convert from './components/Convert';
+
+// const CurrencySwitcher = ({ toCurrency, key }) => (
+//   <select
+//     name='cars'
+//     id='cars'
+//     onChange={(e) => toCurrency(key, e.target.value)}>
+//     <option value='GBP'>GBP</option>
+//     <option value='EUR'>EUR</option>
+//   </select>
+// );
 
 const App = () => {
+  // const [currency, setCurrency] = useState({ to: '', from: '', convertedAmount: '' });
+
+  // const updateCurrency = (key, currency) => {
+  //   setCurrency({ ...currency, [key]: currency });
+  // };
+
+  // const convert = () => {
+  //   //fire endpoint api
+  // }
+
   return (
     <div className='container'>
       <h1 className='display-1 text-center'>Currency Converter</h1>
       <AmountInput />
+      <br />
+      {/* <CurrencySwitcher key={'from'} toCurrency={updateCurrency} />
+      <br />
+      <br />
+      <CurrencySwitcher key={'to'} toCurrency={updateCurrency} />
+      <br />
+      <button onClick={convert}>Convert</button> */}
+      <Currencies />
+      <br />
+      <Currencies />
+      <br />
+      <Convert />
     </div>
   );
 };
---
+++ src/components/Convert/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Convert = () => {
+  return (
+    <div>
+      <button>Convert</button>
+    </div>
+  );
+};
+
+export default Convert;
