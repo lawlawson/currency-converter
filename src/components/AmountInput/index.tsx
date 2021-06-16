87f6d6dc394072a6ab035f317d926bd3a3954cc3
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-06-16 17:54:35 +0100

    Implemented text area and button

--- src/components/AmountInput/index.tsx
+++ src/components/AmountInput/index.tsx
@@ -1,14 +1,28 @@
+import { BsArrowLeftRight } from 'react-icons/bs';
+
 const AmountInput = () => {
   return (
     <div>
       <p>Amount</p>
-      <input
-        type='text'
-        className='form-control'
-        aria-label='Sizing example input'
-        aria-describedby='inputGroup-sizing-default'
-      />
-      <div>Image goes here</div>
+      <div className='row'>
+        <div className='col-10'>
+          <input
+            type='text'
+            className='form-control'
+            aria-label='Sizing example input'
+            aria-describedby='inputGroup-sizing-default'
+          />
+        </div>
+        <div className='col-2'>
+          <BsArrowLeftRight
+            size={40}
+            onClick={() => {
+              alert('clicked');
+            }}
+            style={{ cursor: 'pointer' }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
